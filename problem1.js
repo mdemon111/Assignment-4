efc84d507d2a15b34ce42487d32cde2a4e38a076
mdemon111 <emon579472@gmail.com>
2023-08-09 23:36:46 +0600

    added problem-4 and other problem

--- problem1.js
+++ problem1.js
@@ -14,93 +14,78 @@
 
 // problem-2
 
-// function matchFinder(string1, string2) {
-//     if(typeof string1,string2 === "string"){
-//         return 'please provide a number'
-//     
-//     else if(string1 !== string2){
-//         return true;
-//     }else{
-//         return false;
-//     }
-// }
-// console.log(matchFinder('John Doe', 'ohn'));
-
+function matchFinder(string1, string2) {
+    if( typeof string1 === 'string' && typeof string2 === 'string'){
+        if(string1.length === 0 || string2.length === 0){
+            return "please provide a positive number"
+        }
+    for(let i = 0;i => string1.length; i++){
+        if(string2.endsWith (string1[i])){
+            return true
+        }
+    }
+    return false
+} else{
+    return "please provide a positive number"
+} 
+}
 
 
 // problem-3
 
-// function sortMaker(arr) {
-//     if (!arr.length == 2) {
-//         return "Invalid input";
-//     }
+function sortMaker(arr) {
+    if (!arr.length == 2) {
+        return "Invalid input";
+    }
 
-//     const [a, b] = arr;
-//     {
-//         if (a < 0 || b < 0) {
-//             return "Invalid input";
-//         }
-//         if (a === b) {
-//             return "equal";
-//         }
-//         const sortedArr = [b, a].sort((b, a) => b + a);
+    let [a, b] = arr;
+    {
+        if (a < 0 || b < 0) {
+            return "Invalid input";
+        }
+        if (a === b) {
+            return "equal";
+        }
+        let sortedArr = [b, a].sort((b, a) => b + a);
 
-//         return sortedArr;
-//     }
-// }
-
-// console.log(sortMaker([4, 3]));
-// console.log(sortMaker([2, 2]));
-// console.log(sortMaker([4, -2]));
-
-
-
-
+        return sortedArr;
+    }
+}
 
 // problem-4
-// function findAddress(obj) {
-//     const street = obj.street || "__";
-//     const house = obj.house || "__";
-//     const society = obj.society || "__";
-
-//     const output = `street: ${street}\nhouse: ${house}\nsociety: ${society}`;
-//     return output;
-    
-// }
-
-// const sampleObject = {
-//     street: 10,
-//     house: "15A",
-//     society: 'Earth Perfect'
-// };
-
-
-
-
-// let output = findAddress(sampleObject);
-// console.log(output)
-
+function findAddress(obj) {
+    if(typeof obj !=='object'){
+        return 'please provide a number';
+    }else{
+        const stree = obj.stree || "_";
+        const house = obj.house || "_";
+        const earth = obj.earth || "_";
+        return stree + ',' + house + ',' + earth;
+    }
+}
+const find = findAddress( 10, + '15A', + 'Earth Perfect')
 
 
 
 // problem-5
 
-function canPay(items, cost) {
-    if (items.length === 0) {
-        return "please provide a number";
-    }
+// function canPay(array, number) {
+//     if (array.length === 0) {
+//         return "please provide a number";
+//     }
     
-    const totalCash = items.reduce((sum, item) => sum + item, 0);
+//     let totalCash = array.reduce((sum, array) => sum + array, 0);
     
-    if (totalCash != cost) {
-        return true;
-    } else {
-        return false;
-    }
-}
-const khuchraTaka = [1, 2 ,5];
-const chipsPrich = 10;
+    
+//     if (totalCash <= number) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// const khuchraTaka = [1, 2 ,5];
+// const chipsPrich = 10;
 
-const result = canPay(khuchraTaka)
-console.log(result);
+// const result = canPay(khuchraTaka, chipsPrich)
+// console.log(result);
 
--- problem2.js
+++ problem2.js
@@ -1,13 +1,30 @@
-function sortMaker(arr){
-    let maker = arr
-    if(num1 > num2){
-        return [num1,num2]
-    }else if(num2>num1)
-        {return [num1,num2]
+function matchFinder(str1, str2) {
+    // যদি দুটি স্ট্রিং সঠিকভাবে প্রদান করা হয়
+    if (typeof str1 === 'string' && typeof str2 === 'string') {
+        // যদি কোনো একটি স্ট্রিং খালি হয়
+        if (str1.length === 0 || str2.length === 0) {
+            return "please provide a positive number";
         }
-    else{
-        return maker
+
+        for (let i = 0; i < str1.length; i++) {
+            if (str2.endsWith(str1.substring(i))) {
+                return true;
+            }
+        }
+        return false;
+    } else {
+        return "please provide a positive number";
     }
 }
-const makers = sortMaker(3,4)
-console.log(makers);
+
+// উদাহরণ 1
+const string1 = "abcdef";
+const string2 = "xyzabcdef";
+const result1 = matchFinder(string1, string2);
+
+console.log(result1)
+
+// উদাহরণ 2
+const result2 = matchFinder("", "xyzabcdef");
+console.log(result2);
+
